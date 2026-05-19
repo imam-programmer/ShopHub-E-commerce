c9c23f3cb3f09b3dec1f1d06c1cb63612dc7a4ac
imam-programmer <imamprogrammer09@gmail.com>
2026-05-19 22:26:01 +0600

    header half done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header>
+        <div className="container">
+          <nav>
+            <div className="flex items-center gap-[31.52px]">
+            <img src="./image/logo.png" alt="" />
+            <ul className="flex items-center gap-[31.19px]">
+              <li className="nav-li">Home</li>
+              <li className="nav-li">Categories</li>
+              <li className="nav-li">About</li>
+              <li className="nav-li">Deals</li>
+            </ul>
+            </div>
+            
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
