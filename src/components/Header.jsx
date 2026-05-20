cf7968eedf623f493f45adb362757c5d921e54ac
imam-programmer <imamprogrammer09@gmail.com>
2026-05-20 07:53:21 +0600

    header second part done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <header>
+      <header >
         <div className="container">
-          <nav>
+          <nav className="flex justify-between items-center h-16.25">
             <div className="flex items-center gap-[31.52px]">
             <img src="./image/logo.png" alt="" />
             <ul className="flex items-center gap-[31.19px]">
@@ -15,7 +15,15 @@ const Header = () => {
               <li className="nav-li">Deals</li>
             </ul>
             </div>
-            
+            <div className=" w-lg relative">
+              <i class="fa-solid fa-magnifying-glass absolute left-3 top-4/8 text-[#9CA3AF]  translate-y-[-50%] text-base"></i>
+              <input type="text" placeholder="Search products..." className="search-input"  />
+            </div>
+            <div className="flex items-center gap-4">
+
+            <i class="fa-solid fa-cart-shopping text-[24px]" ></i>
+            <i class="fa-solid fa-circle-user text-[24px]"></i>
+            </div>
           </nav>
         </div>
       </header>
