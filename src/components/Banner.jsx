fd4827416ca9eb09f672bd573437dc05244ab879
imam-programmer <imamprogrammer09@gmail.com>
2026-06-05 00:17:55 +0600

    heru part medium device responsive done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
         <div className="container">
           <div className="banner-container flex justify-between items-center gap-10 md:gap-0  flex-col md:flex-row">
             <div className="banner-text text-center md:text-start">
-              <h1 className='text-[#FFFFFF] leading-15 text-4xl xs:text-[40px] md:text-[60px] font-bold font-inter mb-3 '>Summer Sale</h1>
+              <h1 className='text-[#FFFFFF] leading-15 text-4xl xs:text-[40px] md:text-[45px] lg:text-[60px] font-bold font-inter mb-3 '>Summer Sale</h1>
               <p className='font-inter font-light mb-2 xs:mb-5 mb:mb-8 leading-7 md:text-[20px] text-[#DBEAFE]'>Up to 70% off on selected items. Limited time offer!</p>
               <button className='banner-btn'>Shop Now</button>
             </div>
