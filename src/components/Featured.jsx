05dd1a35da8ef34cbcc3baa3b9fb7bf515247983
imam-programmer <imamprogrammer09@gmail.com>
2026-06-14 22:58:59 +0600

    array through data dynamic done

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import Featubox from './Featubox'
 
 const Featured = () => {
+  const products=[
+    {
+      img:"./image/feature-img-1.png",
+      title:"Wireless Headphones",
+      p:"Premium quality sound",
+      price:"$99.99",
+    }
+  ]
   return (
     <section className='md:pt-16 pt-5 mt-5 md:mt-0 md:h-135 pb-5 md:pb-0  bg-[rgba(229,231,235,.3)]'>
         <div className="container">
@@ -10,7 +18,7 @@ const Featured = () => {
            <button className='text-[#3B82F6] md:text-[16px] text-[12px] font-medium font-inter leading-6' >View All</button>
             </div>
             <div className='grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2  gap-4 xl:gap-4 md:gap-2 justify-between'>
-            <Featubox img="./image/feature-img-1.png" title="Wireless Headphones" p="Premium quality sound" price="$99.99"/>
+            <Featubox img={products.img} title="Wireless Headphones" p="Premium quality sound" price="$99.99"/>
             <Featubox img="./image/feature-img-2.png" title="Smartphone Pro" p="Latest technology" price="$799.99"/>
             <Featubox img="./image/feature-img-3.png" title="Designer Sneakers" p="Comfortable & stylish" price="$129.99"/>
             <Featubox img="./image/feature-img-4.png" title="Gaming Laptop" p="High performance" price="$1,299.99"/>
