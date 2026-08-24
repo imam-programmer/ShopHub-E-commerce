3d0f478de7bdbb6709ec44a6c07b49a0a0cf103c
imam-programmer <imamprogrammer09@gmail.com>
2026-08-24 21:54:05 +0600

    CategoryShowProduct page's search part done

--- src/components/categorypage/CategorySideBar.jsx
+++ src/components/categorypage/CategorySideBar.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CategorySideBar = () => {
+  const [ActiveCategory, setActiveCategory] = useState(false)
   return (
     <div className='w-75 mr-2'>
 
         <h2 className='text-2xl mb-3 font-medium text-teal-700'>Product Categories</h2>
         <ul className='px-2 py-2 border-3 text-teal-700 font-medium border-[#bebebe] rounded-2xl'>
-            <li className='border-3 text-teal-700 font-medium border-[#bebebe] rounded-2xl mb-2 cursor-pointer pl-3 pb-.75'>All Products</li>
-            <li className='border-3 text-teal-700 font-medium border-[#bebebe] rounded-2xl mb-2 cursor-pointer pl-3 pb-.75'>Shirt</li>
-            <li className='border-3 text-teal-700 font-medium border-[#bebebe] rounded-2xl mb-2 cursor-pointer pl-3 pb-.75'>Shirt</li>
-            <li className='border-3 text-teal-700 font-medium border-[#bebebe] rounded-2xl mb-2 cursor-pointer pl-3 pb-.75'>Shirt</li>
-            <li className='border-3 text-teal-700 font-medium border-[#bebebe] rounded-2xl mb-2 cursor-pointer pl-3 pb-.75'>Shirt</li>
+          {Array.from({length:5},()=>(
+
+            <li onClick={()=>setActiveCategory(!ActiveCategory)} className={`border-3 ${ActiveCategory? "bg-amber-600 text-white":'bg-white'} text-teal-700 font-medium border-[#bebebe] rounded-2xl mb-2 cursor-pointer pl-3 py-1`}>All Products</li>
+          ))}
+           
         </ul>
     </div>
   )
